de6a5a5399b9ad33a270b582ee95f5a0777b12bb
IqranNaktare04 <devopsbyiqran04@gmail.com>
2025-03-29 13:07:34 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket"
+resource "aws_s3_bucket" "example_bucket01" {
+  bucket = "swiggy-bucket0111"
 
   # Enable versioning
   versioning {
